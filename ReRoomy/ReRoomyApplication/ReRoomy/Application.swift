ddd031c4ac75bcd7a716a02ca7edaf6c89b1b10c
luximetr <luximetr.sync@gmail.com>
2025-11-18 21:28:19 +0800

    Working on Application layer

---
+++ ReRoomy/ReRoomyApplication/ReRoomy/Application.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class Application: NSObject, UIApplicationDelegate {
+    
+    // MARK: - Life cycle
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
---
+++ ReRoomy/ReRoomyApplication/ReRoomy/main.swift
@@ -0,0 +1,3 @@
+import UIKit
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, NSStringFromClass(UIApplication.self), NSStringFromClass(Application.self))
